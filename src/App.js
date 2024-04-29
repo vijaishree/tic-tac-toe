8eb7d355689f8903ffcd4fd6d7f103060a125146
Vijai Shree <vijaishreeseenivasagan@gmail.com>
2024-04-29 18:49:14 +0530

    creates function to handle click and toggle from null value to X

--- src/App.js
+++ src/App.js
@@ -2,29 +2,38 @@ export default function Board(){
 
   [square,setSquare] = useState(Array(9).fill(null));
 
+  function handleClick(index){
+
+    nextSquare = square.slice();
+
+    nextSquare[i] = "X";
+
+    setSquare(nextSquare);
+  }
+
   return(<>
   <div className="board-row">
-    <Square value={square[0]}/>
-    <Square value={square[1]}/>
-    <Square value={square[2]}/>
+    <Square value={square[0]} onSquareClick = {() => handleClick(0)}/>
+    <Square value={square[1]} onSquareClick = {() => handleClick(1)}/>
+    <Square value={square[2]} onSquareClick = {() => handleClick(2)}/>
   </div>
 
   <div className="board-row">
-    <Square value={square[3]}/>
-    <Square value={square[4]}/>
-    <Square value={square[5]}/>
+    <Square value={square[3]} onSquareClick = {() => handleClick(3)}/>
+    <Square value={square[4]} onSquareClick = {() => handleClick(4)}/>
+    <Square value={square[5]} onSquareClick = {() => handleClick(5)}/>
   </div>
 
   <div className="board-row">
-    <Square value={square[6]}/>
-    <Square value={square[7]}/>
-    <Square value={square[8]}/>
+    <Square value={square[6]} onSquareClick = {() => handleClick(6)}/>
+    <Square value={square[7]} onSquareClick = {() => handleClick(7)}/>
+    <Square value={square[8]} onSquareClick = {() => handleClick(8)}/>
   </div>
   </>)
 }
 
-function Square({value}){
+function Square({value, onSquareClick}){
 
-  return  <buttton className="square" >{value}</buttton>;
+  return  <buttton className="square" onClick = {onSquareClick}>{value}</buttton>;
 
 }
