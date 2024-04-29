b402d5840bfe6dc67db5bf58ada0b1f21c184956
Vijai Shree <vijaishreeseenivasagan@gmail.com>
2024-04-29 18:42:10 +0530

    creates a state for each button and passes the value as prop

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 export default function Board(){
 
+  [square,setSquare] = useState(Array(9).fill(null));
+
   return(<>
   <div className="board-row">
-    <Square />
-    <Square />
-    <Square />
+    <Square value={square[0]}/>
+    <Square value={square[1]}/>
+    <Square value={square[2]}/>
   </div>
 
   <div className="board-row">
-    <Square />
-    <Square />
-    <Square />
+    <Square value={square[3]}/>
+    <Square value={square[4]}/>
+    <Square value={square[5]}/>
   </div>
 
   <div className="board-row">
-    <Square />
-    <Square />
-    <Square />
+    <Square value={square[6]}/>
+    <Square value={square[7]}/>
+    <Square value={square[8]}/>
   </div>
   </>)
 }
 
-function Square(){
+function Square({value}){
 
-  return (<>
-  <buttton className="square" />
-  </>);
+  return  <buttton className="square" >{value}</buttton>;
 
 }
