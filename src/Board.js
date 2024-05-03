2766b9e38346a29cc239778171caebe28652866c
Vijai Shree <vijaishreeseenivasagan@gmail.com>
2024-05-03 13:34:18 +0530

    handles click and introduces states

--- src/Board.js
+++ src/Board.js
@@ -1,22 +1,54 @@
 import Square from './Square.js';
 
+import {useState} from 'react';
+
 export default function Board(){
+
+    const [square,setSquare] = useState(Array(9).fill(null));
+
+    const [XNext , setXNext] = useState(true);
+
+    function handleClick(index){
+
+        const nextSquare = square.slice();
+
+        if(nextSquare[index]) return;
+
+        if(XNext) nextSquare[index] = "X";
+
+        if(!XNext) nextSquare[index] = "O";
+
+        setSquare(nextSquare);
+
+        setXNext(!XNext);
+    }
+
+
     return(<>
         <div className='status' data-testid = 'status'>{GameStatus(square , XNext)}</div> 
+
         <div className="board-row">
+
         <Square value={square[0]} onClick={ () => handleClick(0)}/>
         <Square value={square[1]} onClick={ () => handleClick(1)}/>
         <Square value={square[2]} onClick={ () => handleClick(2)}/>
+
         </div>
+
         <div className="board-row">
+
         <Square value={square[3]} onClick={ () => handleClick(3)}/>
         <Square value={square[4]} onClick={ () => handleClick(4)}/>
         <Square value={square[5]} onClick={ () => handleClick(5)}/>
+
         </div>
+
         <div className="board-row">
+
         <Square value={square[6]} onClick={ () => handleClick(6)}/>
         <Square value={square[7]} onClick={ () => handleClick(7)}/>
         <Square value={square[8]} onClick={ () => handleClick(8)}/>
+        
         </div>
         </>);
         
