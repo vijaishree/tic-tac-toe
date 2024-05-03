131b3bf3aaacf9e0f5bca5bd454ba2c30bf8a0b0
Vijai Shree <vijaishreeseenivasagan@gmail.com>
2024-05-03 13:36:34 +0530

    declares winner

--- src/Board.js
+++ src/Board.js
@@ -12,7 +12,7 @@ export default function Board(){
 
         const nextSquare = square.slice();
 
-        if(nextSquare[index]) return;
+        if(nextSquare[index] || WinningDeclaration(square)) return;
 
         if(XNext) nextSquare[index] = "X";
 
@@ -24,7 +24,7 @@ export default function Board(){
     }
 
     function GameStatus(square , xNext){
-        
+
         let status; 
         
           const winner = WinningDeclaration(square);
@@ -35,7 +35,33 @@ export default function Board(){
         
         return status;
     
+    }
+
+    function WinningDeclaration(square){
+
+        const lines =[
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6]
+        ];
+      
+        for(let i =0 ; i< lines.length ; i++){
+      
+          const [a,b,c] = lines[i];
+      
+          if(square[a] && square[a] === square[b] && square[a]=== square[c]) return square[a];
+      
+         
         }
+        
+        return null ;
+
+    }
 
 
     return(<>
